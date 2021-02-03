fb1b33459eb7e2139126e7e7d5a7b1f087512bf8
Henry <hfang49@yahoo.ca>
2021-02-03 14:57:35 -0600

    Added map to working scene with colliders.

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Controls scene switching
 public class GameManager : MonoBehaviour
 {
-
+    // Loads the game scene
     public void StartGame()
     {
         SceneManager.LoadScene("InventoryTestScene");
     }
 
+    // Exits the game
     public void QuitGame()
     {
         Application.Quit();
