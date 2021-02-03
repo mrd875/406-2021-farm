d54f851d40f66369572b73f54a4205534726aa95
Henry <hfang49@yahoo.ca>
2021-02-03 13:18:29 -0600

    Main menu with scene changing GameManager.

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("InventoryTestScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
