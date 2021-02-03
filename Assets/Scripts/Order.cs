581b9791b71c81cb09d425aa958396f6286bd261
Jordan Ripley <jor498@usask.ca>
2021-02-03 12:33:30 -0600

    Added order system files into master

---
+++ Assets/Scripts/Order.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour
+{
+    public string produceName = "";
+    public int orderAmount = 0;
+
+    public Order(string initName, int initAmount) {
+        produceName = initName;
+        orderAmount = initAmount;
+    }
+
+    public void SetProduceName(string newName) {
+        produceName = newName;
+    }
+
+    public string GetProduceName() {
+        return produceName;
+    }
+
+    public void SetOrderAmount(int newAmount) {
+        orderAmount = newAmount;
+    }
+
+    public int GetOrderAmount() {
+        return orderAmount;
+    }
+}
