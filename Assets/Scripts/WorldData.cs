327ea127bf719949a7e6cc9ac6c4e559101797a4
Tyler Vogel (tjv255) <tjv255@mail.usask.ca>
2021-02-09 11:05:45 -0600

    Created multiple squares for different player territories

--- Assets/Scripts/WorldData.cs
+++ Assets/Scripts/WorldData.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class WorldData : MonoBehaviour
 {
+    // Interactable tilemaps
     static public Tilemap diggableLayer;
     static public Tilemap plantableLayer;
     static public Tilemap highlighter;
 
+    // Areas on the map belonging to each player
+    static public PolygonCollider2D playerOneBoundary;
+    static public PolygonCollider2D playerTwoBoundary;
+    static public PolygonCollider2D playerThreeBoundary;
+    static public PolygonCollider2D playerFourBoundary;
+
+    // Locations where each player spawns after being caught in another's field
     static public Vector2 playerOneSpawnLocation;
     static public Vector2 playerTwoSpawnLocation;
+    static public Vector2 playerThreeSpawnLocation;
+    static public Vector2 playerFourSpawnLocation;
 
     static public List<Vector3Int> plantedLocations;
 
@@ -21,6 +31,14 @@ public class WorldData : MonoBehaviour
         highlighter = GameObject.Find("Highlighter").GetComponent<Tilemap>();
         playerOneSpawnLocation = GameObject.Find("PlayerOneSpawn").transform.position;
         playerTwoSpawnLocation = GameObject.Find("PlayerTwoSpawn").transform.position;
+
+        // Boundary colliders are in order: top to bottom, left to right. 
+        // So 0 is top, 1 is left, 2 is right, 3 is bottom: ❖
+        PolygonCollider2D[] boundries = GameObject.Find("Boundries").GetComponents<PolygonCollider2D>();
+        playerOneBoundary = boundries[1];
+        playerTwoBoundary = boundries[0];
+        playerThreeBoundary = boundries[2];
+        playerFourBoundary = boundries[3];
 
 
         //Used to tell if a plant has been planted at location before, so seed isn't consumed
