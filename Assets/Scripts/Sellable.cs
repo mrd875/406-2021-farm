349fb1ed1bf84cf1e47f2f80271b8858c86afed3
Max Dale <maxkodrago@gmail.com>
2021-02-11 13:25:19 -0600

    Hooked up orders to plant selling

--- Assets/Scripts/Sellable.cs
+++ Assets/Scripts/Sellable.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class Sellable : MonoBehaviour
 {
     public int sellPrice;
+    public OrderSystem orders;
+    private Item inventoryInfo;
+
+    private string itemName;
+
+    void Start()
+    {
+        inventoryInfo = this.GetComponent<Item>();
+        orders = FindObjectOfType<OrderSystem>();
+        itemName = inventoryInfo.itemName.Substring(9);
+    }
 
     public void SellPlant()
     {
         PlayerData.AddMoney(sellPrice);
+        (bool, int) orderCheck = orders.CheckTickets(itemName);
 
+        if (orderCheck.Item1)
+        {
+            orders.UpdateTicket(itemName, orderCheck.Item2);
+        }
     }
 }
