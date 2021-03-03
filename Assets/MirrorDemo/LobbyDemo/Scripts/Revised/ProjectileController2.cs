f11b55989aabbff8fc9d2a92f172c7b7f02b9bdb
Tyler Vogel (tjv255) <tjv255@mail.usask.ca>
2021-03-02 20:10:13 -0600

    Added projectile mechanic

---
+++ Assets/MirrorDemo/LobbyDemo/Scripts/Revised/ProjectileController2.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Controller used for individual projectiles
+public class ProjectileController2 : MonoBehaviour
+{
+    public Vector2 target; // where the projectile should move to
+    public Vector2 startLocation; // where the projectile is launched
+    public Vector2 startSpeed;  // The speed of the launcher slightly affects the speed of the projectile
+    public float speed = 10;    // Speed of the projectile
+
+    public float lifeTime = 0.5f;   // how long the projectile will remain airborn for
+    public float speedReduction = 0.5f; // default reduction in movement speed to target hit
+
+    [HideInInspector]
+    public string parentTagName; // must be set after instantiation
+
+    private Vector2 direction; // Direction for projectile to move
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (target - startLocation).normalized;
+      
+        // bullet is destroyed after a given time after not colliding with anything
+        Destroy(this.gameObject, lifeTime);
+    }
+
+    private void Update()
+    {
+        // Object moves slightly faster in the direction it is thrown
+        transform.Translate((direction * speed + (startSpeed / 2)) * Time.deltaTime);
+        if (gameObject.tag == "Untagged")
+        {
+            if (parentTagName == "PlayerOne") { gameObject.tag = "PlayerOneProjectile"; }
+            else if (parentTagName == "PlayerTwo") { gameObject.tag = "PlayerTwoProjectile"; }
+            else if (parentTagName == "PlayerThree") { gameObject.tag = "PlayerThreeProjectile"; }
+            else if (parentTagName == "PlayerFour") { gameObject.tag = "PlayerFourProjectile"; }
+        }
+        Debug.Log(gameObject.tag);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Player one projectile hits player two
+        if (gameObject.tag == "PlayerOneProjectile" && (other.tag == "PlayerTwo" || other.tag == "PlayerThree" || other.tag == "PlayerFour")
+            && !other.gameObject.GetComponent<PlayerTouch>().inHomeZone)
+        {
+            other.GetComponent<PlayerMovement2>().ReduceSpeed(speedReduction);
+            Destroy(this.gameObject);
+        }
+        else if (gameObject.tag == "PlayerTwoProjectile" && (other.tag == "PlayerOne" || other.tag == "PlayerThree" || other.tag == "PlayerFour")
+            && !other.gameObject.GetComponent<PlayerTouch>().inHomeZone)
+        {
+            other.GetComponent<PlayerMovement2>().ReduceSpeed(speedReduction);
+            Destroy(this.gameObject);
+        }
+
+        // Projectile should not be destroyed when collision occurs with the one who launches it
+        else if (gameObject.tag == "PlayerOneProjectile" && other.tag == "PlayerOne") { }
+        else if (gameObject.tag == "PlayerTwoProjectile" && other.tag == "PlayerTwo") { }
+
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
---
+++ Assets/MirrorDemo/LobbyDemo/Scripts/Revised/Shoot2.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+// Shoots projectile: used in throwing water balloon
+public class Shoot2 : NetworkBehaviour
+{
+    public GameObject projectilePrefab; // prefab to be launched as projectile: must have projectile controller script
+    public float projectileSpeed = 10f;   // speed of projectile
+    public float shotCooldown = 1f; // cooldown before fireing another projectile
+
+    [HideInInspector]
+    public bool canShoot = true;
+    [HideInInspector]
+    public bool isShooting = false;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Q) && canShoot)
+        {
+            isShooting = true;
+            StartCoroutine(ShotCooldown(shotCooldown));
+            if (isServer)
+            {
+                RpcSpawnProjectile(gameObject.transform.position, gameObject.GetComponent<Rigidbody2D>().velocity, gameObject.tag);
+            }
+            else
+            {
+                CmdSpawnProjectile(gameObject.transform.position, gameObject.GetComponent<Rigidbody2D>().velocity, gameObject.tag);
+            }
+        }
+        else
+        {
+            isShooting = false;
+        }
+    }
+
+    // Activates shot cooldown, a period which no shots can be fired in
+    private IEnumerator ShotCooldown(float cooldown)
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
+    }
+
+/*    // Creates the projectile and sends it moving
+    private void SpawnProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, gameObject.transform.position, gameObject.transform.rotation);
+        (projectile.GetComponent("ProjectileController2") as ProjectileController2).parentTagName = gameObject.tag;
+        (projectile.GetComponent("ProjectileController2") as ProjectileController2).startLocation = gameObject.transform.position;
+        (projectile.GetComponent("ProjectileController2") as ProjectileController2).startSpeed = gameObject.GetComponent<Rigidbody2D>().velocity;
+        *//*        (projectile.GetComponent("ProjectileController") as ProjectileController).speedReduction = speedReduction;*//* // Edit with special launcher??
+    }*/
+
+    [Command]
+    private void CmdSpawnProjectile(Vector2 startLocation, Vector2 startSpeed, string tag)
+    {
+        RpcSpawnProjectile(startLocation, startSpeed, tag);
+    }
+
+    // Creates a projectile and sends it moving
+    [ClientRpc]
+    private void RpcSpawnProjectile(Vector2 startLocation, Vector2 startSpeed, string tag)
+    {
+        GameObject projectile = Instantiate(projectilePrefab, gameObject.transform.position, gameObject.transform.rotation);
+        (projectile.GetComponent("ProjectileController2") as ProjectileController2).parentTagName = tag;
+        (projectile.GetComponent("ProjectileController2") as ProjectileController2).startLocation = startLocation;
+        (projectile.GetComponent("ProjectileController2") as ProjectileController2).startSpeed = startSpeed;
+    }
+}
