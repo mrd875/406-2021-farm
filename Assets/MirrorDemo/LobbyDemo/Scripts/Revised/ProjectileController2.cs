5edb2955e3b718d0223d7d8cf0c9246527469b4c
Tyler Vogel (tjv255) <tjv255@mail.usask.ca>
2021-03-18 18:06:13 -0600

    Update ProjectileController2.cs

--- Assets/MirrorDemo/LobbyDemo/Scripts/Revised/ProjectileController2.cs
+++ Assets/MirrorDemo/LobbyDemo/Scripts/Revised/ProjectileController2.cs
@@ -37,11 +37,9 @@ public class ProjectileController2 : MonoBehaviour
 
     private void Update()
     {
-        transform.rotation = parentRotation;    // reset rotation that translate works properly
+        transform.rotation = parentRotation;    // Temporarily reset rotation that translate works properly
         transform.Translate((direction * speed + (startSpeed / 2)) * Time.deltaTime);   // Object moves slightly faster in the direction it is thrown
         transform.rotation = rotation;
-        
-
     }
 
     void OnTriggerEnter2D(Collider2D other)
