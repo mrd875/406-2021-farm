c1861c456556ce34c42addf0060bd14b19d21b45
Max Dale <maxkodrago@gmail.com>
2021-03-25 13:06:01 -0600

    Icon implemented and How-to constructed

--- Assets/Scripts/GameManager/PauseMenu.cs
+++ Assets/Scripts/GameManager/PauseMenu.cs
@@ -31,14 +31,14 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+        //Time.timeScale = 1f;
         isPaused = false;
     }
 
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f;
+        //Time.timeScale = 0f;
         isPaused = true;
     }
 }
